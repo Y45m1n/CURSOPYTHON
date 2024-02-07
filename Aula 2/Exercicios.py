64d6433fa79bed120d9e8b7881df4cf94b54529f
Y45m1n <yasmindeoliveira286@gmail.com>
2024-02-07 16:57:06 -0300

    edit

---
+++ Aula 2/Exercicios.py
@@ -0,0 +1,112 @@
+# # 1 - Variáveis e Operações com Números:
+
+# # soma
+# print("soma")
+# a = float(input ("digite um numero"))
+# b = float(input ("digite um numero"))
+# resultado = a + b
+# print(resultado)
+
+# # subtração
+# print("subtração")
+# a = float(input ("digite um numero"))
+# b = float(input ("digite um numero"))
+# resultado = a - b
+# print(resultado)
+
+# # multiplicação
+# print("multiplicação")
+# a = float(input ("digite um numero"))
+# b = float(input ("digite um numero"))
+# resultado = a * b
+# print(resultado)
+
+# # divisão
+# print("divisão")
+# a = float(input ("digite um numero"))
+# b = float(input ("digite um numero"))
+# resultado = a / b
+# print(resultado)
+
+# # divisão inteira
+# print("divisão inteira")
+# a = float(input ("digite um numero"))
+# b = float(input ("digite um numero"))
+# resultado = a // b
+# print(resultado)
+
+# # resto da divisão 
+# print("resto da divisão ")
+# a = float(input ("digite um numero"))
+# b = float(input ("digite um numero"))
+# resultado = a % b
+# print(resultado)
+
+# # potenciacao
+# print("potenciacao")
+# a = float(input ("digite um numero"))
+# b = float(input ("digite um numero"))
+# resultado = a ** b
+# print(resultado)
+
+
+# area = 10
+# resultado = (3.14 * area)**2
+# print(resultado)
+
+# 2 - Manipulação de Strings:
+
+# nome = input(" Digite seu nome ")
+# sobrenome = input (" Digite seu sobrenome ")
+# nomeCompleto = nome +" "+sobrenome
+# print(nomeCompleto)
+
+# nomeCompleto_lower = nomeCompleto.lower()
+# print(nomeCompleto_lower)
+
+# nomeCompleto_upper = nomeCompleto.upper()
+# print(nomeCompleto_upper)
+
+# nomeCompleto_substituido = nomeCompleto.replace("lima", "oliveira")
+# print(nomeCompleto_substituido)
+
+# 3 - Utilização de Listas e Tuplas:
+
+# cores = ["azul", "vermelho", "amarelo"]
+# cores.append("verde")
+# cores.append("laranja")
+
+# print(cores)
+
+
+# cordenadas = (409.98 , 50.988)
+# print("Latitude ", cordenadas[0])
+# print("Longitude ", cordenadas[1])
+
+# 4 - Uso de Operadores Lógicos em Estruturas Condicionais:
+
+# tem_sol = True
+# tem_chuva = False 
+
+# if tem_sol and not tem_chuva:
+#     print("O dia está agradável")
+# else:
+#     print("O dia não está agradável")
+
+# numero1 = float(input("Digite o primeiro número"))
+# numero2 = float(input("Digite o segundo número"))
+# if numero1 % 2 == 0:
+#         print("Os dois números são pares")
+# else:
+#         print("Um dos números não é par")
+
+idade = print(float(input("Digite sua idade:")))
+
+if idade >= 18 and idade <= 65 :
+    certo = True
+else:
+    certo = False
+
+print(certo)
+
+
